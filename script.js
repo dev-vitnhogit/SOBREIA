83c4b1535bf1cae006e34087216cdc9fbc264e9e
dev-vitnhogit <devvitinhogit@gmail.com>
2026-01-17 21:47:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function comprar() {
+  alert("Aqui você pode integrar com Hotmart, Kiwify ou outro checkout.");
+}
+
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
